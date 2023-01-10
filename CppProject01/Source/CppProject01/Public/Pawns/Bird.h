88f054cca56c267a1d77065f3df5007807a2f73a
iamjungdamin <76206124+iamjungdamin@users.noreply.github.com>
2023-01-10 23:45:08 +0900

    Create Pawn

---
+++ CppProject01/Source/CppProject01/Public/Pawns/Bird.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Bird.generated.h"
+
+// 전방선언 Forward Declaration, 클래스 유형을 정의하지 않고 선언함
+// (1) 실제로 필요한 헤더 파일만 인클루드 함으로써 code bloat 방지
+// (2) 두 헤더 파일이 서로 인클루드 하는 Circluar Dependency 방지
+class UCapsuleComponent;
+class USkeletalMeshComponent;
+
+UCLASS()
+class CPPPROJECT01_API ABird : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ABird();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UCapsuleComponent* Capsule;
+
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent* BirdMesh;
+};
