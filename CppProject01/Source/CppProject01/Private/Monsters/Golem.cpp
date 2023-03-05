53a024402187334bc26407f14f26cb6346e754bb
iamjungdamin <76206124+iamjungdamin@users.noreply.github.com>
2023-03-06 05:23:13 +0900

    Create Golem

---
+++ CppProject01/Source/CppProject01/Private/Monsters/Golem.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Monsters/Golem.h"
+#include "UObject/ConstructorHelpers.h"
+
+AGolem::AGolem()
+{
+	ConstructorHelpers::FObjectFinder<USkeletalMesh>MeshAsset(TEXT("/Game/Blueprints/Monsters/Meshes/Golem/Mutant"));
+	if (MeshAsset.Succeeded()) {
+		GetMesh()->SetSkeletalMesh(MeshAsset.Object);
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Failed to Load Assets"));
+	}
+
+	ConstructorHelpers::FObjectFinder<UMaterial>MaterialAsset(TEXT("/Game/Blueprints/Monsters/Meshes/Golem/mutant_M"));
+	if (MaterialAsset.Succeeded()) {
+		GetMesh()->SetMaterial(0, MaterialAsset.Object);
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Failed to Load Assets"));
+	}
+
+	GetMesh()->SetRelativeLocationAndRotation(FVector(0.f, 0.f, -90.f), FRotator(0.f, -90.f, 0.f));
+}
---
+++ CppProject01/Source/CppProject01/Public/Monsters/Golem.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Monsters/BaseMonster.h"
+#include "Golem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPPROJECT01_API AGolem : public ABaseMonster
+{
+	GENERATED_BODY()
+	
+public:
+	AGolem();
+};
